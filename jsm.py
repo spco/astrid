b6d651ebe035f2aca20b4b2d37eff443bf55e055
Sam Cox <sc676@le.ac.uk>
2018-04-20 15:39:32 +0100

    Complete submission, but only using echo rather than qsub.

--- jsm.py
+++ jsm.py
@@ -1,102 +1,80 @@
 import argparse
 import json
-
+import subprocess
 # Reads in a json file. Each item in the json file is a job to submit. The contents are its name, and which jobs
 # it depends upon. The program takes the jobs, submits them to torque, and captures the jobIDs returned.
 # These are then used as inputs for the jobs which are dependent.
 
-x = 0
-
-
-def submit_job_and_return_next_jobid(job_name, graph, jobids):
-    global x
-    x += 1
-    labels = [item[0] for item in graph[:]]
-    dependencies = [item for item in [subitem[0] for subitem in graph[:]]
-                    if graph[labels.index(job_name)][labels.index(item)+1] == 1]
-    print("submitted " + job_name + " as job id " + str(x) + ", dependent on jobs ", [(dep, jobids[dep]) for dep in dependencies])
-    return x
-
-
-def submit_jobs(graph, data):
-    submitted_jobids = dict([])
-    for label in data:
-        submitted_jobids[label] = submit_job_and_return_next_jobid(label, graph, submitted_jobids)
-
-    print(submitted_jobids)
-    return 0
-
-
-# Based upon the inputted json, create a directed graph of job dependencies.
-def generate_graph(data):
-    twodarray = []
-    list_of_labels = []
-    blank_list = [[] for _ in range(len(data))]
-    for label in data:
-        list_of_labels.append(label)
-        twodarray.append([label] + blank_list)
-        if data[label]["depends_on"] == "":
-            print(label + " is root node")
-
-    for i,label in enumerate(data):
-        # print(label, data[label]['depends_on'].split(','))
-        for item in data[label]['depends_on'].split(','):
-            if item != "":
-                twodarray[list_of_labels.index(label)][list_of_labels.index(item)+1] = 1
-
-    print("twodarray:", twodarray)
-    print("labels:", list_of_labels)
-    return twodarray
-
 y = 0
 
-def submitjob(stage,dependency_ids):
+
+def submit_job(stage, dependency_ids):
     global y
+    # increment the jobid - this is a mock part since I don't get anything back from echo but do from qsub
     y += 1
-    #submit(stage,dependency_ids)
+    if dependency_ids:
+        command = 'echo "-W depend=afterok:' + ':'.join([str(item) for item in dependency_ids]) + '"'
+    else:
+        command = 'echo'
+    print(command)
+    # Execute command in bash
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    if error is not None:
+        print("Abort - something went wrong in submitting!")
+        print("Stage name:", stage, "dependent on", dependency_ids)
+        exit(1)
+    print("submitting " + str(stage) + " with job id " + str(y) + ", dependent on jobs", dependency_ids)
     return y
 
 
-def attempt_to_submit_next_stage(index, stages, submitted_stages, data):
+def attempt_to_submit_next_stage(index, stages, submitted_stages, data, jobids):
     if stages[index] in submitted_stages:
         # this stage already submitted - move on
-        print("already submitted stage " + str(index) + ": " + stages[index])
+        # print("already submitted stage " + str(index) + ": " + stages[index])
         return
     else:
-        # look at the dependencies of the index-th stage. If all are in submitted_stages, this is ready for submit. Otherwise, move on.
+        # look at the dependencies of the index-th stage. If all are in submitted_stages, this is ready for submit.
+        # Otherwise, move on.
         dependencies = data[stages[index]]["depends_on"].split(",")
-        # print(dependencies)
-        if set(dependencies).issubset(submitted_stages):
+        # print('dependencies:', dependencies)
+        if set(dependencies).issubset(set(submitted_stages)):
+            if dependencies != [""]:
+                dep_indices = [jobids[stages.index(item)] for item in dependencies]
+            else:
+                dep_indices = []
+            # print("can submit stage " + str(index) + ": " + stages[index] + " with dependency job ids", dep_indices)
             # submit stage
-            print("can submit stage " + str(index) + ": " + stages[index])
+            jobids[index] = submit_job(stages[index], dep_indices)
             submitted_stages.append(stages[index])
             return
         else:
-            print("cannot yet submit stage " + str(index) + ": " + stages[index])
+            print("cannot yet submit stage " + str(index) + ": " + stages[index] + ", because of dependency on " +
+                  ', '.join([str(item) for item in set(dependencies).difference(submitted_stages)]))
     return
 
 
-def jsm(args):
-    print("hello")
+def djs(args):
     json_data = open(args.input_file).read()
 
     data = json.loads(json_data)
-    print(data)
 
     list_of_stages = [stages for stages in data]
+    print("Stages to be submitted:")
     print(list_of_stages)
+    jobids = ["" for _ in list_of_stages]
     list_of_submitted_stages = [""]
     index = -1
     second_index = 0
-    while len(list_of_submitted_stages) < len(list_of_stages) + 1 and second_index < 20:
+    # Loop while there are un-submitted jobs, or until we loop through len(list_of_stages)^2 times,
+    # in which case we must have hit an unresolvable dependency
+    while len(list_of_submitted_stages) < len(list_of_stages) + 1 and second_index < len(list_of_stages)*len(list_of_stages):
         index += 1
         index = index % len(list_of_stages)
-        print(index)
         second_index += 1
-        attempt_to_submit_next_stage(index, list_of_stages, list_of_submitted_stages, data)
+        attempt_to_submit_next_stage(index, list_of_stages, list_of_submitted_stages, data, jobids)
 
-    print(list_of_stages)
-    print(list_of_submitted_stages)
+    print("\nAll jobs submitted!")
 
 
 def create_parser():
@@ -109,4 +87,4 @@ def create_parser():
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    jsm(args)
+    djs(args)
